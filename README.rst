8a450f6acd8c84c5f8a19fa0c7dcb9b074a08828
ti250 <ti250@cam.ac.uk>
2016-07-12 14:51:05 +0100

    Add files via upload

---
+++ README.rst
@@ -0,0 +1,11 @@
+######
+PyCav
+######
+
+PyCav is a python physics simulation library which provides simple classes and functions so that simulations can be created quickly and easily.
+
+
+Installation
+=============
+
+For installation instructions and requirements, see the INSTALL file.
---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='PyCav',
+
+    version='1.0.0a1',
+
+    description='PyCav module',
+    long_description=long_description,
+
+    url=' ',
+
+    author='PyCav team 2016',
+    author_email=' ',
+
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Education',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering :: Physics'
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='physics simulation education',
+
+    packages=['pycav'],
+
+    install_requires=['numpy>=1.1'],
+
+)
