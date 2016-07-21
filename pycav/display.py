6a94c2e0b466fce13aabaa81112dc42f5c906372
aidancrilly <aidancrilly@hotmail.co.uk>
2016-07-21 15:39:04 +0100

    cleared up usage and increased OS support

--- pycav/display.py
+++ pycav/display.py
@@ -10,19 +10,34 @@ VIDEO_TAG = """<video controls>
  Your browser does not support the video tag.
 </video>"""
 
-def anim_to_html(anim):
-    if not hasattr(anim, '_encoded_video'):
+def anim_to_html(anim,temp,fname,overwrite):
+    if temp:
         with NamedTemporaryFile(suffix='.mp4',delete = False) as f:
             anim.save(f.name, fps=20, extra_args=['-vcodec', 'libx264'])
             video = open(f.name, "rb").read()
             os.unlink(f.name)
             assert not os.path.exists(f.name)
-        anim._encoded_video = base64.b64encode(video).decode('utf-8')
+    else:
+        if not os.path.exists(fname):
+            anim.save(fname, fps=20, extra_args=['-vcodec', 'libx264'])
+        elif os.path.exists(fname) and overwrite:
+        	anim.save(fname, fps=20, extra_args=['-vcodec', 'libx264'])
+        video = open(fname, "rb").read()
+    encoded_video = base64.b64encode(video).decode('utf-8')
     
-    return VIDEO_TAG.format(anim._encoded_video)
+    anim._encoded_video = encoded_video
 
+    return anim
 
+def display_animation(fname):
+    if hasattr(fname,'_encoded_video'):
+    	return HTML(VIDEO_TAG.format(fname._encoded_video))
+    else:
+        video = open(fname, "rb").read()
+        encoded_video = base64.b64encode(video).decode('utf-8')
+        return HTML(VIDEO_TAG.format(encoded_video))
 
-def display_animation(anim):
+def create_animation(anim, temp = False, fname = 'video.mp4', overwrite = True):
     plt.close(anim._fig)
-    return HTML(anim_to_html(anim))
+    anim = anim_to_html(anim,temp,fname,overwrite)
+    return anim
