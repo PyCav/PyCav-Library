8474da9f05a49e6cfc9fb39d110775dadce26542
Taketomo <ti250@cam.ac.uk>
2016-07-22 11:48:41 +0100

    Update __init__.py

--- pycav/__init__.py
+++ pycav/__init__.py
@@ -2,3 +2,18 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+def green():
+	import random
+
+	from urllib.request import urlopen
+	from bs4 import BeautifulSoup
+
+	url = "http://www.srcf.ucam.org/davegreenfacts/"
+	html = urlopen(url)
+
+	soup = BeautifulSoup(html, 'html.parser')
+	list = soup.find_all('li')
+
+	print(random.choice(list).text.replace('Dave Green', "\033[0;32mDave Green\033[0m"))
